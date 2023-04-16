810742fb69c1b54fbecb5514eb084a62a8838e26
tonight11 <godofspeedman@gmail.com>
2023-04-17 01:17:03 +0300

    feat: setting up quasar

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<NuxtLoadingIndicator
+			color="repeating-linear-gradient(to right,rgb(144, 170, 255) 0%,rgb(72, 115, 255) 100%)"
+		/>
+		<NuxtLayout>
+			<q-btn
+				color="red"
+				icon="mail"
+				icon-right="send"
+				label="On Left and Right"
+			/>
+			<q-btn flat style="color: #ff0080" label="Fuchsia Flat" />
+			<q-btn style="background: goldenrod; color: white" label="Goldenrod" />
+			<q-btn outline style="color: goldenrod" label="Goldenrod" />
+			<NuxtPage />
+		</NuxtLayout>
+	</div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div>
+		<q-card class="my-card">
+			<img src="https://cdn.quasar.dev/img/mountains.jpg" />
+
+			<q-card-section>
+				<div class="text-h6">Our Changing Planet</div>
+				<div class="text-subtitle2">by John Doe</div>
+			</q-card-section>
+
+			<q-card-section class="q-pt-none">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+				sapiente eligendi sit ea excepturi, quasi consequuntur, nulla eum ex
+				commodi fugiat libero ad. Delectus dolor corrupti dolorum, impedit
+				suscipit fugit!
+			</q-card-section>
+		</q-card>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+	.my-card {
+		max-width: 250px;
+		width: 100%;
+	}
+</style>
