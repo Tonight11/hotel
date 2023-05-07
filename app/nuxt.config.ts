f91a7f950d83f80def71d0a1b8f3cb073dcc4777
tonight11 <godofspeedman@gmail.com>
2023-05-08 00:38:21 +0300

    chore: setting up nuxt-image module

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,7 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	app: { pageTransition: { name: 'page', mode: 'out-in' } },
-	modules: ['nuxt-quasar-ui'],
+	modules: ['nuxt-quasar-ui', '@nuxt/image-edge'],
+	image: {
+		screens: {
+			xs: 320,
+			sm: 640,
+			md: 768,
+			lg: 1080,
+		},
+	},
 	quasar: {
 		extras: {
 			fontIcons: ['material-icons'],
