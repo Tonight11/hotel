a19ec260fcd7f242d1988b0ebc7f90c9c109788a
tonight11 <godofspeedman@gmail.com>
2023-05-08 15:24:30 +0300

    feat: single hotel page 65%/100%

---
+++ app/components/SingleHotel/About.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div class="single-hotel__about">
+		<nuxt-img sizes="sm:100vw lg:70vw" src="/hello.jpeg" />
+		<div class="single-hotel__text">
+			Жилой комплекс "Sky Dive" расположен в центре Дубая. Это лучшее место для
+			тех, кто ищет баланс между работой и отдыхом. Уникальный архитектурный
+			стиль жилого комплекса "Sky Dive" придаст новую достопримечательность
+			нашему городу.
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped></style>
