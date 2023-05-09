b72396a77dfdec67b174cb5345bcdb180a0ed681
shiftnn <chocho2c@yandex.ru>
2023-05-09 13:04:47 +0300

    feat: title components & styles

---
+++ app/components/titles/pageTitle.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="wrapper-page">
+        <div class="page-lato" >
+            <slot name="lato"></slot>
+        </div>
+        <div class="cursive" >
+            <slot name="cursive"></slot>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+    .cursive {
+        margin-top: -6.5rem;
+    }
+    @import url(@/assets/styles/titleComp.scss);
+</style>
