ab8fc00fc7133e23997b5f1ce65538890c22e9dc
tonight11 <godofspeedman@gmail.com>
2023-05-20 13:17:22 +0300

    added prettier ignore

--- components/UI/Menu.vue
+++ components/UI/Menu.vue
@@ -1,23 +1,49 @@
 <template>
     <div class="burger-wrapper">
-        <div class="burger-wrapper__navigation" :class="isActive ? 'activeNavigation' : ''">
+        <div
+            class="burger-wrapper__navigation"
+            :class="isActive ? 'activeNavigation' : ''"
+        >
             <div class="burger-wrapper__navigation-menu">
-                <div class="burger-wrapper__navigation-links" @mouseleave="mouseLeaveHandler">
-                    <NuxtLink to="/" class="linkFont" :class="isActive ? 'textActive' : 'textInactive'" @click="menuToggle"
-                        @mouseover="mouseOverHandler('Home')">
-                        Home</NuxtLink>
-                    <NuxtLink to="/hotel" class="linkFont" :class="isActive ? 'textActive' : 'textInactive'"
-                        @click="menuToggle" @mouseover="mouseOverHandler('Hotel')">Hotel
+                <div
+                    class="burger-wrapper__navigation-links"
+                    @mouseleave="mouseLeaveHandler"
+                >
+                    <NuxtLink
+                        to="/"
+                        class="linkFont"
+                        :class="isActive ? 'textActive' : 'textInactive'"
+                        @click="menuToggle"
+                        @mouseover="mouseOverHandler('Home')"
+                    >
+                        Home</NuxtLink
+                    >
+                    <NuxtLink
+                        to="/hotel"
+                        class="linkFont"
+                        :class="isActive ? 'textActive' : 'textInactive'"
+                        @click="menuToggle"
+                        @mouseover="mouseOverHandler('Hotel')"
+                        >Hotel
                     </NuxtLink>
-                    <NuxtLink to="/hotel/123" class="linkFont" :class="isActive ? 'textActive' : 'textInactive'"
-                        @click="menuToggle" @mouseover="mouseOverHandler('Shotel')">SHotel
+                    <NuxtLink
+                        to="/hotel/123"
+                        class="linkFont"
+                        :class="isActive ? 'textActive' : 'textInactive'"
+                        @click="menuToggle"
+                        @mouseover="mouseOverHandler('Shotel')"
+                        >SHotel
                     </NuxtLink>
                 </div>
                 <div class="burger-wrapper__navigation-displayer">
                     <titles-page-title>
                         <template #cursive>
-                            <p class="displayFont" :class="isActive ? 'textActive' : 'textInactive'
-                                ">
+                            <p
+                                class="displayFont"
+                                :class="
+                                    isActive ? 'textActive' : 'textInactive'
+                                "
+                            >
                                 {{ textToDisplay }}
                             </p>
                         </template>
@@ -25,11 +51,20 @@
                 </div>
             </div>
             <div class="burger-wrapper__navigation-footer">
-                <div class="burger-wrapper__navigation-footer-item" :class="isActive ? 'textActive' : 'textInactive'">
+                <div
+                    class="burger-wrapper__navigation-footer-item"
+                    :class="isActive ? 'textActive' : 'textInactive'"
+                >
                     <p>INSTAGRAM</p>
-                    <img src="@/assets/imgs/social_media/instagram.svg" alt="" />
+                    <img
+                        src="@/assets/imgs/social_media/instagram.svg"
+                        alt=""
+                    />
                 </div>
-                <div class="burger-wrapper__navigation-footer-item" :class="isActive ? 'textActive' : 'textInactive'">
+                <div
+                    class="burger-wrapper__navigation-footer-item"
+                    :class="isActive ? 'textActive' : 'textInactive'"
+                >
                     <p>TELEGRAM</p>
                     <img src="@/assets/imgs/social_media/telegram.svg" alt="" />
                 </div>
@@ -37,11 +72,20 @@
         </div>
         <div class="burger-wrapper__menu" @click="menuToggle">
             <div class="burger-wrapper__menu-dot"></div>
-            <div class="burger-wrapper__menu-dot" :class="isActive ? 'activeDotClose' : ''"></div>
-            <div class="burger-wrapper__menu-dot" :class="isActive ? 'activeDotLong' : ''"></div>
+            <div
+                class="burger-wrapper__menu-dot"
+                :class="isActive ? 'activeDotClose' : ''"
+            ></div>
+            <div
+                class="burger-wrapper__menu-dot"
+                :class="isActive ? 'activeDotLong' : ''"
+            ></div>
             <div class="burger-wrapper__menu-dot"></div>
             <div class="burger-wrapper__menu-dot"></div>
-            <div class="burger-wrapper__menu-dot" :class="isActive ? 'activeDotClose' : ''"></div>
+            <div
+                class="burger-wrapper__menu-dot"
+                :class="isActive ? 'activeDotClose' : ''"
+            ></div>
             <div class="burger-wrapper__menu-dot"></div>
             <div class="burger-wrapper__menu-dot"></div>
             <div class="burger-wrapper__menu-dot"></div>
