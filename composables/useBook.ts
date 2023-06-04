a1b156f79eee5ae1aa161e6493eb63e70130f734
tonight11 <godofspeedman@gmail.com>
2023-06-05 00:34:45 +0300

    refector: added generics

--- composables/useBook.ts
+++ composables/useBook.ts
@@ -1,28 +1,35 @@
-export const useBook = () => {
+// move to pinia => bookStore.ts
+
+export const useBook = <T extends Date | null>() => {
     const email = ref('')
     const name = ref('')
     const number = ref('')
     const guest = ref(0)
 
     const dateStart = ref(new Date())
-    const dateEnd = ref()
+    const dateEnd = ref<T>(null as T)
     const dayStart = computed(() => dateStart.value.getDate())
     const monthStart = computed(() => dateStart.value.getMonth() + 1)
     const yearStart = computed(() => dateStart.value.getFullYear())
-    const dayEnd = ref(null)
-    const monthEnd = ref(null)
-    const yearEnd = ref(null)
-    const handleDate = (modelData: Array<Date>): void => {
-        console.log(modelData)
+    const dayEnd = ref<number | null>(null)
+    const monthEnd = ref<number | null>(null)
+    const yearEnd = ref<number | null>(null)
+
+    const updateEndDateValues = (date: Date) => {
+        const dateEndRef = toRef(dateEnd, 'value')
+        dateEndRef.value = date
+        dayEnd.value = dateEndRef.value.getDate()
+        monthEnd.value = dateEndRef.value.getMonth() + 1
+        yearEnd.value = dateEndRef.value.getFullYear()
+    }
+
+    const handleDate = (modelData: [Date, T?]): void => {
         dateStart.value = modelData[0]
         if (modelData[1]) {
-            dateEnd.value = modelData[1]
-            dayEnd.value = dateEnd.value.getDate()
-            monthEnd.value = dateEnd.value.getMonth() + 1
-            yearEnd.value = dateEnd.value.getFullYear()
+            updateEndDateValues(modelData[1])
         }
     }
-    const disabledDates = computed((dates) => {
+    const disabledDates = computed((dates: Date[]): Date[] => {
         const today = new Date()
 
         const tomorrow = new Date(today)
@@ -31,9 +38,7 @@ export const useBook = () => {
         const afterTomorrow = new Date(tomorrow)
         afterTomorrow.setDate(tomorrow.getDate() + 1)
 
-        dates = [tomorrow, afterTomorrow]
-
-        return dates
+        return [tomorrow, afterTomorrow]
     })
     return {
         email,
